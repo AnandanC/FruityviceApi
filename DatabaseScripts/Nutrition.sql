7c8fa42fedb3b7ba4f1f4f2c8ba8d9de29add1d1
AnandanC <anandan643@gmail.com>
2023-05-04 18:06:54 +0530

    Update Nutrition.sql

--- DatabaseScripts/Nutrition.sql
+++ DatabaseScripts/Nutrition.sql
@@ -1,9 +1,20 @@
 
 DROP TABLE IF EXISTS tblFruits;
 CREATE TABLE IF NOT EXISTS tblFruits(
-   Genus  VARCHAR(32)
-  ,Name   VARCHAR(64)
-  ,Id     INTEGER  NOT NULL PRIMARY KEY IDENTITY(1,1)
-  ,Family VARCHAR(32)
-  ,Order  VARCHAR(32)
+  FruitId      INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  , Genus      VARCHAR(32)
+  ,Name        VARCHAR(64)
+  ,Family      VARCHAR(32)
+  ,OrderDesc   VARCHAR(32)
+);
+
+DROP TABLE IF EXISTS tblNutritions;
+CREATE TABLE IF NOT EXISTS tblNutritions(
+   NutritionId    INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,FruitId        INTEGER
+  ,Carbohydrates  DECIMAL(5,2)
+  ,Protein        DECIMAL(5,2)
+  ,Fat            DECIMAL(5,2)
+  ,Calories       DECIMAL(5,2)
+  ,Sugar          DECIMAL(5,2)
 );
